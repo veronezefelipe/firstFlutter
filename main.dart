65bf1a5861e572975207bfb55f3fb3289a6092f3
veronezefelipe <96065689+veronezefelipe@users.noreply.github.com>
2023-07-19 22:39:52 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Center(
+      child: Text(
+        'Vero',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.pink, fontSize: 50.0),
+      ),
+    ),
+  ));
+}
